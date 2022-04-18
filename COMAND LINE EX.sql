a631bc699f1e47352ff9b245045bf36ad87def9d
Lucas <l.o.bastos@live.com>
2022-04-17 21:24:56 -0300

    commit inicial

---
+++ COMAND LINE EX.sql
@@ -0,0 +1,122 @@
+
+CREATE TABLE funcionario (
+                cpf CHAR(11) NOT NULL,
+                primeiro_nome VARCHAR(15) NOT NULL,
+                nome_meio CHAR(1) NOT NULL,
+                ultimo_nome VARCHAR(15) NOT NULL,
+                data_nascimento DATE NOT NULL,
+                endereco VARCHAR(30) NOT NULL,
+                sexo CHAR(1) NOT NULL,
+                salario DECIMAL(10,2) NOT NULL,
+                cpf_supervisor CHAR(11) NOT NULL,
+                numero_departamento INT NOT NULL,
+                Parent_cpf CHAR(11) NOT NULL,
+                PRIMARY KEY (cpf)
+);
+
+ALTER TABLE funcionario MODIFY COLUMN cpf CHAR(11) COMMENT 'Primary Key em funcionario, no atributo cpf.';
+
+ALTER TABLE funcionario MODIFY COLUMN primeiro_nome VARCHAR(15) COMMENT 'primeiro nome do funcionario';
+
+ALTER TABLE funcionario MODIFY COLUMN Parent_cpf CHAR(11) COMMENT 'Primary Key em funcionario, no atributo cpf.';
+
+
+CREATE TABLE departamento (
+                numero_departamento INT NOT NULL,
+                nome_departamento VARCHAR(15) NOT NULL,
+                cpf_gerente CHAR(11) NOT NULL,
+                data_inicio_gerente DATE NOT NULL,
+                PRIMARY KEY (numero_departamento)
+);
+
+
+CREATE UNIQUE INDEX departamento_idx
+ ON departamento
+ ( nome_departamento );
+
+CREATE UNIQUE INDEX departamento_idx1
+ ON departamento
+ ( cpf_gerente );
+
+CREATE TABLE localizacoes_departamento (
+                numero_departamento INT NOT NULL,
+                local VARCHAR(15) NOT NULL,
+                PRIMARY KEY (numero_departamento, local)
+);
+
+
+CREATE TABLE projeto (
+                numero_projeto INT NOT NULL,
+                nome_projeto VARCHAR(15) NOT NULL,
+                local_projeto VARCHAR(15) NOT NULL,
+                numero_departamento INT NOT NULL,
+                PRIMARY KEY (numero_projeto)
+);
+
+
+CREATE UNIQUE INDEX projeto_idx
+ ON projeto
+ ( nome_projeto );
+
+CREATE TABLE trabalha_em (
+                cpf_funcionario CHAR(11) NOT NULL,
+                numero_projeto INT NOT NULL,
+                horas DECIMAL(3,1) NOT NULL,
+                PRIMARY KEY (cpf_funcionario, numero_projeto)
+);
+
+
+CREATE TABLE dependente (
+                cpf_funcionario CHAR(11) NOT NULL,
+                nome_dependente VARCHAR(15) NOT NULL,
+                cpf CHAR(11) NOT NULL,
+                sexo CHAR(1) NOT NULL,
+                data_nascimento DATE NOT NULL,
+                parentesco VARCHAR(15) NOT NULL,
+                PRIMARY KEY (cpf_funcionario, nome_dependente)
+);
+
+ALTER TABLE dependente MODIFY COLUMN cpf CHAR(11) COMMENT 'Primary Key em funcionario, no atributo cpf.';
+
+
+ALTER TABLE funcionario ADD CONSTRAINT funcionario_funcionario_fk
+FOREIGN KEY (Parent_cpf)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE dependente ADD CONSTRAINT funcionario_dependente_fk
+FOREIGN KEY (cpf)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE departamento ADD CONSTRAINT funcionario_departamento_fk
+FOREIGN KEY (cpf_gerente)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE trabalha_em ADD CONSTRAINT funcionario_trabalha_em__fk
+FOREIGN KEY (cpf_funcionario)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE projeto ADD CONSTRAINT departamento_projeto_fk
+FOREIGN KEY (numero_departamento)
+REFERENCES departamento (numero_departamento)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE localizacoes_departamento ADD CONSTRAINT departamento_localizacoes_departamento_fk
+FOREIGN KEY (numero_departamento)
+REFERENCES departamento (numero_departamento)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE trabalha_em ADD CONSTRAINT projeto_trabalha_em__fk
+FOREIGN KEY (numero_projeto)
+REFERENCES projeto (numero_projeto)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
