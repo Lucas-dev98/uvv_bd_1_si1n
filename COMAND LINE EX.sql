58884b8c2b403b698ccc630296b9e593a395bfc3
Lucas <l.o.bastos@live.com>
2022-04-21 14:09:50 -0300

    corrigindo relacionamento entre funcionario e departamento

--- COMAND LINE EX.sql
+++ COMAND LINE EX.sql
@@ -7,214 +7,193 @@ CREATE DATABASE PSET1_ELMASLI;
 /* Selecionando o Banco de Dados Criado*/
 
  USE PSET1_ELMASLI;
- 
+
 /* Criando a Tabela "funcionario":
 	definindo as restriçoes de NOT NULL, definindo tipo dos campos,
     e adicionando o campo "cpf", como uma chave primaria .
 */
+
 CREATE TABLE funcionario (
                 cpf CHAR(11) NOT NULL,
                 primeiro_nome VARCHAR(15) NOT NULL,
-                nome_meio CHAR(1) NOT NULL,
+                nome_meio CHAR(1),
                 ultimo_nome VARCHAR(15) NOT NULL,
-                data_nascimento DATE NOT NULL,
-                endereco VARCHAR(30) NOT NULL,
-                sexo CHAR(1) NOT NULL,
-                salario DECIMAL(10,2) NOT NULL,
+                data_nascimento DATE,
+                endereco VARCHAR(50),
+                sexo CHAR(1),
+                salario DECIMAL(10,2),
                 cpf_supervisor CHAR(11) NOT NULL,
                 numero_departamento INT NOT NULL,
                 PRIMARY KEY (cpf)
 );
 
-/* _________________________________________________________________________________________________________________*/
+ALTER TABLE funcionario COMMENT 'Tabela que armazena as informações dos funcionários.';
 
-/*  Alteração da tabela "funcionario", adicionando o comentario no atributo "cpf".  */
-	ALTER TABLE funcionario MODIFY COLUMN cpf CHAR(11) COMMENT 'Primary Key em funcionario, no atributo cpf.';
+ALTER TABLE funcionario MODIFY COLUMN cpf CHAR(11) COMMENT 'CPF do funcionário. Será a PK da tabela.';
 
-/* _________________________________________________________________________________________________________________*/
+ALTER TABLE funcionario MODIFY COLUMN primeiro_nome VARCHAR(15) COMMENT 'Primeiro nome do funcionário.';
 
-/*  Alteração da tabela "funcionario", adicionando o comentario no atributo "primeiro_nome".         */
-	ALTER TABLE funcionario MODIFY COLUMN primeiro_nome VARCHAR(15) COMMENT 'primeiro nome do funcionario';
+ALTER TABLE funcionario MODIFY COLUMN nome_meio CHAR(1) COMMENT 'Inicial do nome do meio.';
 
-/* _________________________________________________________________________________________________________________*/
+ALTER TABLE funcionario MODIFY COLUMN ultimo_nome VARCHAR(15) COMMENT 'Sobrenome do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN data_nascimento DATE COMMENT 'Data de nascimento do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN endereco VARCHAR(30) COMMENT 'Endereço do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN sexo CHAR(1) COMMENT 'Sexo do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN salario DECIMAL(10, 2) COMMENT 'Salário do funcionário.';
+
+ALTER TABLE funcionario MODIFY COLUMN numero_departamento INTEGER COMMENT 'Número do departamento do funcionário.';
 
 
- /* Criando a tabela de "dependente":
-	definindo as restriçoes de NOT NULL, definindo tipo dos campos,
-    e adicionando o campo "cpf_funcionario, nome_dependente", como uma chave primaria .
-*/
-	CREATE TABLE dependente (
+CREATE TABLE dependente (
                 cpf_funcionario CHAR(11) NOT NULL,
                 nome_dependente VARCHAR(15) NOT NULL,
-                sexo CHAR(1) NOT NULL,
-                data_nascimento DATE NOT NULL,
-                parentesco VARCHAR(15) NOT NULL,
+                sexo CHAR(1),
+                data_nascimento DATE,
+                parentesco VARCHAR(15),
                 PRIMARY KEY (cpf_funcionario, nome_dependente)
 );
 
-/* _________________________________________________________________________________________________________________*/
+ALTER TABLE dependente COMMENT 'Tabela que armazena as informações dos dependentes dos funcionários.';
 
-/* criando a tabela "departamento":
-	definindo as restriçoes de NOT NULL, definindo tipo dos campos,
-    e adicionando o campo "numero_departamento", como uma chave primaria .
-*/
-	CREATE TABLE departamento (
+ALTER TABLE dependente MODIFY COLUMN cpf_funcionario CHAR(11) COMMENT 'para a tabela funcionário.';
+
+ALTER TABLE dependente MODIFY COLUMN nome_dependente VARCHAR(15) COMMENT 'Nome do dependente. Faz parte da PK desta tabela.';
+
+ALTER TABLE dependente MODIFY COLUMN sexo CHAR(1) COMMENT 'Sexo do dependente.';
+
+ALTER TABLE dependente MODIFY COLUMN data_nascimento DATE COMMENT 'Data de nascimento do dependente.';
+
+ALTER TABLE dependente MODIFY COLUMN parentesco VARCHAR(15) COMMENT 'Descrição do parentesco do dependente com o funcionário.';
+
+
+CREATE TABLE departamento (
                 numero_departamento INT NOT NULL,
                 nome_departamento VARCHAR(15) NOT NULL,
                 cpf_gerente CHAR(11) NOT NULL,
-                data_inicio_gerente DATE NOT NULL,
+                data_inicio DATE,
                 PRIMARY KEY (numero_departamento)
 );
 
-/* _________________________________________________________________________________________________________________*/
+ALTER TABLE departamento COMMENT 'Tabela que armazena as informaçoẽs dos departamentos.';
 
-/* Criando a restrição de chave unica em "departamento" no atributo "nome_departamento" . */
-	CREATE UNIQUE INDEX departamento_idx
-		ON departamento
-		( nome_departamento );
- 
-  /* _________________________________________________________________________________________________________________*/
+ALTER TABLE departamento MODIFY COLUMN numero_departamento INTEGER COMMENT 'Número do departamento. É a PK desta tabela.';
 
+ALTER TABLE departamento MODIFY COLUMN nome_departamento VARCHAR(15) COMMENT 'Nome do departamento. Deve ser único.';
 
- /* Criando a restrição de chave unica em "departamento" no atributo "nome_departamento" .  */
-	CREATE UNIQUE INDEX departamento_idx1
-	ON departamento
-	( cpf_gerente );
- 
-/* _________________________________________________________________________________________________________________*/
+ALTER TABLE departamento MODIFY COLUMN cpf_gerente CHAR(11) COMMENT 'onários.';
+
+ALTER TABLE departamento MODIFY COLUMN data_inicio DATE COMMENT 'Data do início do gerente no departamento.';
 
 
+CREATE UNIQUE INDEX departamento_idx
+ ON departamento
+ ( nome_departamento );
 
- /* Criando a tabela de "localizacoes_departamento":
-	definindo as restriçoes de NOT NULL, definindo tipo dos campos,
-    e adicionando o campo "numero_departamento", "local", como uma chave primaria .
-*/
-	CREATE TABLE localizacoes_departamento (
-                numero_departamento INT NOT NULL,
-                local VARCHAR(15) NOT NULL,
-                PRIMARY KEY (numero_departamento, local)
-);
-
-
-/* _________________________________________________________________________________________________________________*/
-
-
-/* criando a tabela "projeto":
-	definindo as restriçoes de NOT NULL, definindo tipo dos campos,
-    e adicionando o campo "numero_projeto", como uma chave primaria .
-*/
-	CREATE TABLE projeto (
+CREATE TABLE projeto (
                 numero_projeto INT NOT NULL,
                 nome_projeto VARCHAR(15) NOT NULL,
-                local_projeto VARCHAR(15) NOT NULL,
+                local_projeto VARCHAR(15),
                 numero_departamento INT NOT NULL,
                 PRIMARY KEY (numero_projeto)
 );
 
-/* _________________________________________________________________________________________________________________*/
+ALTER TABLE projeto COMMENT 'Tabela que armazena as informações sobre os projetos dos departamentos.';
+
+ALTER TABLE projeto MODIFY COLUMN numero_projeto INTEGER COMMENT 'Número do projeto. É a PK desta tabela.';
+
+ALTER TABLE projeto MODIFY COLUMN nome_projeto VARCHAR(15) COMMENT 'Nome do projeto. Deve ser único.';
+
+ALTER TABLE projeto MODIFY COLUMN local_projeto VARCHAR(15) COMMENT 'Localização do projeto.';
 
 
- /* Criando a restrição de chave unica em "projeto" no atributo "nome_projeto" .  */
-	CREATE UNIQUE INDEX projeto_idx
-	ON projeto
-	( nome_projeto );
- 
- 
- 
-  /* _________________________________________________________________________________________________________________*/
- 
- /* Criando a tabela de "trabalha_em":
-	definindo as restriçoes de NOT NULL, definindo tipo dos campos,
-    e adicionando o campo "cpf_funcionario, numero_projeto", como uma chave primaria .
-*/
-	CREATE TABLE trabalha_em (
+CREATE INDEX projeto_idx
+ ON projeto
+ ( nome_projeto );
+
+CREATE UNIQUE INDEX projeto_idx1
+ ON projeto
+ ( nome_projeto );
+
+CREATE TABLE trabalha_em (
                 cpf_funcionario CHAR(11) NOT NULL,
                 numero_projeto INT NOT NULL,
                 horas DECIMAL(3,1) NOT NULL,
                 PRIMARY KEY (cpf_funcionario, numero_projeto)
 );
 
+ALTER TABLE trabalha_em COMMENT 'Tabela para armazenar quais funcionários trabalham em quais projetos.';
 
-  /* _________________________________________________________________________________________________________________*/
+ALTER TABLE trabalha_em MODIFY COLUMN cpf_funcionario CHAR(11) COMMENT 'para a tabela funcionário.';
 
-/* Alternado a tabela "departamento", adicionando a restrição/ relacionamento de Chave Estrangeira,
-	em "cpf_gerente", que faz referência a chave primaria, "cpf", da tabela "funcionario".
-*/
-	ALTER TABLE departamento ADD CONSTRAINT funcionario_departamento_fk
-	FOREIGN KEY (cpf_gerente)
-	REFERENCES funcionario (cpf)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+ALTER TABLE trabalha_em MODIFY COLUMN numero_projeto INTEGER COMMENT 'ara a tabela projeto.';
+
+ALTER TABLE trabalha_em MODIFY COLUMN horas DECIMAL(3, 1) COMMENT 'Horas trabalhadas pelo funcionário neste projeto.';
 
 
-  /* _________________________________________________________________________________________________________________*/
+CREATE TABLE localizacoes_departamento (
+                numero_departamento INT NOT NULL,
+                local VARCHAR(15) NOT NULL,
+                PRIMARY KEY (numero_departamento, local)
+);
 
-/* Alternado a tabela "trabalha_em", adicionando a restrição/ relacionamento de Chave Estrangeira,
-	em "cpf_funcionario", que faz referência a chave primaria, "cpf", da tabela "funcionario".
-*/
-	ALTER TABLE trabalha_em ADD CONSTRAINT funcionario_trabalha_em__fk
-	FOREIGN KEY (cpf_funcionario)
-	REFERENCES funcionario (cpf)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+ALTER TABLE localizacoes_departamento COMMENT 'Tabela que armazena as possíveis localizações dos departamentos.';
 
-  /* _________________________________________________________________________________________________________________*/
-/* Alternado a tabela "funcionario", adicionando a restrição/ relacionamento de Chave Estrangeira,
-	em "cpf_supervisor", que faz referência a chave primaria, "cpf", da tabela "funcionario".
-*/
-	ALTER TABLE funcionario ADD CONSTRAINT funcionario_funcionario_fk
-	FOREIGN KEY (cpf_supervisor)
-	REFERENCES funcionario (cpf)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+ALTER TABLE localizacoes_departamento MODIFY COLUMN numero_departamento INTEGER COMMENT 'm é uma FK para a tabela departamento.';
 
-  /* _________________________________________________________________________________________________________________*/
-/* Alternado a tabela "dependente", adicionando a restrição/ relacionamento de Chave Estrangeira,
-	em "cpf_funcionario", que faz referência a chave primaria, "cpf", da tabela "funcionario".
-*/
-	ALTER TABLE dependente ADD CONSTRAINT funcionario_dependente_fk
-	FOREIGN KEY (cpf_funcionario)
-	REFERENCES funcionario (cpf)
-	ON DELETE NO ACTION
-	ON UPDATE NO ACTION;
+ALTER TABLE localizacoes_departamento MODIFY COLUMN local VARCHAR(15) COMMENT 'Localização do departamento. Faz parte da PK desta tabela.';
 
-  /* _________________________________________________________________________________________________________________*/
 
-/* Alternado a tabela "projeto", adicionando a restrição/ relacionamento de Chave Estrangeira,
-	em "numero_departamento", que faz referência a chave primaria, "numero_departamento", da tabela "departamento".
-*/
-ALTER TABLE projeto ADD CONSTRAINT departamento_projeto_fk
-FOREIGN KEY (numero_departamento)
-REFERENCES departamento (numero_departamento)
+ALTER TABLE departamento ADD CONSTRAINT funcionario_departamento_fk
+FOREIGN KEY (cpf_gerente)
+REFERENCES funcionario (cpf)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
+ALTER TABLE dependente ADD CONSTRAINT funcionario_dependente_fk
+FOREIGN KEY (cpf_funcionario)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-  /* _________________________________________________________________________________________________________________*/
+ALTER TABLE trabalha_em ADD CONSTRAINT funcionario_trabalha_em_fk
+FOREIGN KEY (cpf_funcionario)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
+ALTER TABLE funcionario ADD CONSTRAINT funcionario_funcionario_fk
+FOREIGN KEY (cpf_supervisor)
+REFERENCES funcionario (cpf)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-/* Alternado a tabela "localizacoes_departamento", adicionando a restrição/ relacionamento de Chave Estrangeira,
-	em "numero_departamento", que faz referência a chave primaria, "numero_departamento", da tabela "departamento".
-*/
 ALTER TABLE localizacoes_departamento ADD CONSTRAINT departamento_localizacoes_departamento_fk
 FOREIGN KEY (numero_departamento)
 REFERENCES departamento (numero_departamento)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
+ALTER TABLE projeto ADD CONSTRAINT departamento_projeto_fk
+FOREIGN KEY (numero_departamento)
+REFERENCES departamento (numero_departamento)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-  /* _________________________________________________________________________________________________________________*/
+ALTER TABLE funcionario ADD CONSTRAINT departamento_funcionario_fk
+FOREIGN KEY (numero_departamento)
+REFERENCES departamento (numero_departamento)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-/* Alternado a tabela "trabalha_em", adicionando a restrição/ relacionamento de Chave Estrangeira,
-	em "numero_projeto", que faz referência a chave primaria, "numero_projeto", da tabela "projeto".
-*/
-ALTER TABLE trabalha_em ADD CONSTRAINT projeto_trabalha_em__fk
+ALTER TABLE trabalha_em ADD CONSTRAINT projeto_trabalha_em_fk
 FOREIGN KEY (numero_projeto)
 REFERENCES projeto (numero_projeto)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
-
-/* _________________________________________________________________________________________________________________*/
 
 /* Inserindo os dados nas TUPLAS, das TABELAS . */
 
@@ -223,10 +202,9 @@ ON UPDATE NO ACTION;
 
 
 /* TUPLA JOÃO */
-	INSERT INTO funcionario VALUES (
-		"12345678966", "João","B","Silva","1965-01-09",
-		"Rua das Flores,751, São Paulo,SP","M",30.000,
-		"33344555587", 5
+	insert INTO funcionario values(
+    "12345678966", "joão","B","Silva","Rua das Flores,751,São Paulo,SP",
+    "M",30.000,"3345555587",5
     );
     
 /* _________________________________________________________________________________________________________________*/
@@ -287,13 +265,15 @@ ON UPDATE NO ACTION;
       INSERT INTO funcionario VALUES (
 		"88866555576", "Jorge","E","Brito","1937-11-10",
 		"Rua do Horto,35,São Paulo,SP","M",55.000,
-		"", 1
-    );
-    
-  
+		"", 1);
+        
+        
+   
+        
     
 
 
+
     
     
-    
+   
